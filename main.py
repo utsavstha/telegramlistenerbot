04a9af4cfb3817538e8e10051709744680d141ae
Utsav Shrestha <utsavshrestha@Utsavs-MacBook-Pro.local>
2021-02-11 18:22:48 +0545

    added a message

--- main.py
+++ main.py
@@ -1,24 +1,11 @@
 from telethon import TelegramClient, events, sync
 from telethon.sessions import StringSession
 
-# These example values won't work. You must get your own api_id and
-# api_hash from https://my.telegram.org, under API Development.
 api_id = 2942575
 api_hash = '742735ff0f8b0e6c3e6153ab4bcd2bb3'
-# 1625942892:AAHa52g-krqaqkiIxLNv9UV_ZZNdjeRZwNY
 client = TelegramClient(StringSession("1ApWapzMBu6oZjd2igalo0i3TiJbKqa-L01W9FgNSSo9yuhP5sXLdAF62XfxTcuAxjke3nm6cngPcDTiZ9YMx81gjPVO5MlVfgfpT1_aAV4Ef3KyZ1Fd6TiUwQblWdrj-AvFd9luZRxMRsK5Q1paFv-4snWaS_4LC8JhLcCCw74KN9eGuKQvOWvyoiOOzrQlSdOeMoVsd2p2dmRieUOP_SqHsX2eDEN4DBZ-gPleT7WTJI3AMt4Pb7_oaMX7Fv5csOETXKsfcFOx7ur4lxILz0y8n_9acZPv2_ceFXuZuYXEvfOkOxM6aR9lUgo6u4LYn7wh17C5Z-87aAJQehRGRURw8WRD_eVg="), api_id, api_hash)
 client.start()    
-print(client.session.save())
-#+970569937656
-# client.run_until_disconnected()
-# async def main():
-#     # You can print the message history of any chat:
-#     async for message in client.iter_messages('utsavstha'):
-#         if message.text is None:
-#             continue 
-#         elif "profit" in message.text:
-#CPLleaks
-#                 print(message.sender.username, message.text)
+print("SERVER STARTED")
 
 
 @client.on(events.NewMessage(chats="shehabtelegram"))
