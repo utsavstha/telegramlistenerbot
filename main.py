0bb7b2d2e9c25216f97628ab53ba7a5ec39b609f
Utsav Shrestha <utsavshrestha@Utsavs-MacBook-Pro.local>
2021-02-12 13:51:20 +0545

    removed other channel

--- main.py
+++ main.py
@@ -20,7 +20,6 @@ async def my_event_handler(event):
     print("new event")
     if "حماس" in event.raw_text or "غزة" in event.raw_text or "فلسطين" in event.raw_text or "فلسطيني" in event.raw_text or "فلسطينيين" in event.raw_text or "فلسطينيون" in event.raw_text or "الموطن الفلسطيني" in event.raw_text or "وطنية" in event.raw_text or "قطاع غزة" in event.raw_text or "أزمة" in event.raw_text or "حصار" in event.raw_text or "جهاد" in event.raw_text or "جهاد اسلامي فلسطيني" in event.raw_text or "اسلام" in event.raw_text:
             print("Posted from gazanewsnow")
-            await client.send_message('alshabakia', event.raw_text)
             await client.send_message('CPLleaks', event.raw_text)
 
 @client.on(events.NewMessage(chats="khnmedia"))
